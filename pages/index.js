98fe7eb91eed08d393ceae956d096ac5f547860d
EdZ543 <zhuang.eddie@gmail.com>
2021-11-26 22:58:48 -0500

    Update home page

--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,14 @@ export default function Home() {
         <div className="flex items-center w-full">
           <Cool3DText />
         </div>
-        
+
         <div className="flex flex-col justify-center w-full max-w-lg">
           <p className="sm:text-lg text-sm text-center">
             I&apos;m a high school student who really likes computer science and math.
             I spend most of my time working on Machine Learning, Web Development, and Game Development projects.
             I also enjoy competitive programming and contest math.
-            Some of my other hobbies include anime, youtube, and video games.
+            Some of my other hobbies include anime, youtube, and video games.<br /><br />
+            This website was from scratch with Next.js (React) and Tailwind CSS.
           </p>
 
           <Link href="/projects" passHref>
