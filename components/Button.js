7de6eb799f26cb25a6747f4b6e42d7c09df550e7
EdZ543 <zhuang.eddie@gmail.com>
2021-11-27 11:35:08 -0500

    Update class

--- components/Button.js
+++ components/Button.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Button({ href, children }) {
   return (
     <Link href={href} passHref>
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex items-center">
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex items-center">
         {children}
       </button>
     </Link>
