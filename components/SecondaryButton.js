48774afec032d2f8eeabb8a62584b42e6f3ec0d7
EdZ543 <zhuang.eddie@gmail.com>
2021-11-28 12:30:27 -0500

    Update class name

--- components/SecondaryButton.js
+++ components/SecondaryButton.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function SecondaryButton({ href, children }) {
   return (
-    <button class="bg-transparent hover:bg-blue-500 text-blue-500 font-semibold hover:text-gray-50 py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+    <button className="bg-transparent hover:bg-blue-500 text-blue-500 font-semibold hover:text-gray-50 py-2 px-4 border border-blue-500 hover:border-transparent rounded">
       {children}
     </button>
   )
