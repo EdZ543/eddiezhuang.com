0d2d646730a357158e349147ab344850913251df
EdZ543 <53500062+EdZ543@users.noreply.github.com>
2022-06-09 14:48:24 -0400

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -9,8 +9,8 @@ export default function Contact(){
         <div className="flex sm:flex-row flex-col sm:items-start items-center sm:gap-10">
           <div className="sm:w-1/3 w-full sm:text-left text-center">
             <h1 className="text-4xl mb-2"><b>Hire Me!</b></h1>
-            <p className="flex align-middle items-center sm:justify-start justify-center"><AiFillMail className="text-2xl m-2 flex-none"/>zhuang.eddie@gmail.com</p>
-            <p className="flex align-middle items-center sm:justify-start justify-center"><AiFillPhone className="text-2xl m-2 flex-none"/>416-902-7518</p>
+            <p className="flex align-middle items-center sm:justify-start justify-center"><AiFillMail className="text-2xl m-2 flex-none"/>zhuang [dot] eddie [at] gmail.com</p>
+            <p className="flex align-middle items-center sm:justify-start justify-center"><AiFillPhone className="text-2xl m-2 flex-none"/>416 [slash] 902 [slash] 7518</p>
           </div>
           
           <div className="sm:w-2/3 w-full">
